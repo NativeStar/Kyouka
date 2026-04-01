9ec42db404158f65f943d063eb12cb974f94d6f2
NativeStar <z2858470935@163.com>
2026-04-01 17:18:39 +0800

    设置页支持输入框控件

--- src/options.ts
+++ src/options.ts
@@ -1,4 +1,5 @@
 let audioContext = new AudioContext();
+let tempedConfig: { [key: string]: string | boolean };
 class OptionCheckBox extends HTMLElement {
     constructor() {
         super();
@@ -19,18 +20,56 @@ class OptionCheckBox extends HTMLElement {
         });
     }
 }
-async function playButtonCheckChangeAudio(isEnable:boolean) {
+class OptionSelect extends HTMLElement {
+    constructor() {
+        super();
+        const targetSettingKey = this.getAttribute("settingKey")!;
+        const selectElement = document.createElement("select");
+        selectElement.id = targetSettingKey!;
+        const labelElement = document.createElement("label");
+        labelElement.htmlFor = targetSettingKey!;
+        labelElement.textContent = this.getAttribute("description")!;
+        const defaultOption = document.createElement("option");
+        defaultOption.textContent = "不设置";
+        defaultOption.setAttribute("optionValue", "unset");
+        selectElement.append(defaultOption);
+        const parsedOptions: string[] = JSON.parse(this.getAttribute("optionsList")!);
+        for (const optionItem of parsedOptions) {
+            const [optionItemKey, optionItemName] = optionItem.split(":") as [string, string];
+            const optionElement = document.createElement("option");
+            optionElement.textContent = optionItemName;
+            optionElement.setAttribute("optionValue", optionItemKey);
+            selectElement.append(optionElement);
+        }
+        selectElement.addEventListener("change", () => {
+            const targetOption = selectElement.options[selectElement.selectedIndex]!;
+            const targetOptionKey = targetOption.getAttribute("optionValue")!;
+            chrome.storage.local.set({ [targetSettingKey]: targetOptionKey });
+            playButtonCheckChangeAudio(targetOptionKey!=="unset");
+        });
+        this.append(labelElement, selectElement);
+        getChromeConfig<string>(targetSettingKey, "unset").then(result => {
+            parsedOptions.forEach(optionItem => {
+                const [optionItemKey, optionItemName] = optionItem.split(":") as [string, string];
+                if (optionItemKey === result) {
+                    selectElement.value = optionItemName;
+                }
+            });
+        });
+    }
+}
+async function playButtonCheckChangeAudio(isEnable: boolean) {
     if (audioContext.state === 'suspended') {
         await audioContext.resume();
     }
     const firstOscillator = audioContext.createOscillator();
     const gainNode = audioContext.createGain();
     firstOscillator.type = 'triangle';
-    firstOscillator.frequency.value = isEnable?550:920;
+    firstOscillator.frequency.value = isEnable ? 550 : 920;
     firstOscillator.connect(gainNode);
     const lastOscillator = audioContext.createOscillator();
     lastOscillator.type = 'triangle';
-    lastOscillator.frequency.value = isEnable?920:550;
+    lastOscillator.frequency.value = isEnable ? 920 : 550;
     lastOscillator.connect(gainNode);
     gainNode.connect(audioContext.destination);
     gainNode.gain.setValueAtTime(0.12, audioContext.currentTime);
@@ -40,7 +79,9 @@ async function playButtonCheckChangeAudio(isEnable:boolean) {
     lastOscillator.stop(audioContext.currentTime + 0.16);
 }
 async function getChromeConfig<T = any>(key: string, defaultValue: T): Promise<T> {
-    const result = await chrome.storage.local.get(key);
+    const result = tempedConfig ?? await chrome.storage.local.get<{ [key: string]: boolean | string }>(key);
+    //进行配置缓存
+    if (!tempedConfig) tempedConfig = result;
     if (Reflect.has(result, key)) {
         return result[key] as T;
     } else {
@@ -48,4 +89,5 @@ async function getChromeConfig<T = any>(key: string, defaultValue: T): Promise<T
     }
 }
 customElements.define("option-checkbox", OptionCheckBox);
-addEventListener("contextmenu", e => e.preventDefault())
+customElements.define("option-select", OptionSelect);
+// addEventListener("contextmenu", e => e.preventDefault())
